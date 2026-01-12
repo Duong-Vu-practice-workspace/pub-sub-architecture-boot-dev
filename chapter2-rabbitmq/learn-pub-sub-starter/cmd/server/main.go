401ea2364224c53f00a8eaf644da43919c3ad77f
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-12 10:13:40 +0700

    chapter 3 publisher and queues - l1 publisher and exchanges

--- chapter2-rabbitmq/learn-pub-sub-starter/cmd/server/main.go
+++ chapter2-rabbitmq/learn-pub-sub-starter/cmd/server/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 import (
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -19,6 +22,16 @@ func main() {
 	}
 	defer con.Close()
 	fmt.Println("connected to RabbitMQ")
+	newChannel, err := con.Channel()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create channel: %v\n", err)
+	}
+	data := routing.PlayingState{IsPaused: true}
+	err = pubsub.PublishJSON(newChannel, routing.ExchangePerilDirect, routing.PauseKey, data)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	fmt.Println("sending success")
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
